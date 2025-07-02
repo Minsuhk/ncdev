ab1aaee37ba8fbb40faea637ac975480f6fbbca6
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2025-07-02 07:19:11 -0700

    • attempt to fix the resume button

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -178,7 +178,7 @@ export default function Scene() {
         { type: 'linkedin', url: 'https://www.linkedin.com/in/nicholasmchung/', icon: '/assets/images/icons8-linkedin.svg' },
         { type: 'github',   url: 'https://github.com/Minsuhk',  icon: '/assets/images/icons8-github.svg'   },
         { type: 'email',    url: 'mailto: nicholasmchung@gmail.com',      icon: '/assets/images/icons8-email-100.png'    },
-        { type: 'resume',    url: "public/Nick's Resume (Updated 05.05.25) - UpdatedMK.pdf",      icon: '/assets/images/icons8-resume-100.png'    },
+        { type: 'resume',    url: "Resume.pdf",      icon: '/assets/images/icons8-resume-100.png'    },
       ]
     },
     {
